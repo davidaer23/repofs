903f9b96b67e6009d192d54ba5518257b15e2ca0
daespinosa4 <davidandres102@hotmail.com>
2022-08-07 10:54:41 -0500

    actualizando carpetas

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,73 @@
+const Header = (props) => {
+  return(
+    <div>
+      <h1>{props.course.name}</h1>
+    </div>
+  )
+}
+
+const Part = (props) =>{
+  return (
+    <>
+      <p>{props.name} {props.exercises}</p>
+    </>
+  )
+}
+
+
+const Content = (props) => {
+  
+    return(
+      <>
+< Part name={props.parts.parts[0].name} exercises={props.parts.parts[0].exercises}  />
+< Part name={props.parts.parts[1].name} exercises={props.parts.parts[1].exercises}  />
+< Part name={props.parts.parts[2].name} exercises={props.parts.parts[2].exercises}  />
+</>
+    )
+  
+  
+}
+
+
+const Total = (props) => {
+  let total=0;
+  props.parts.parts.forEach(part => {
+    total+=part.exercises;
+  });
+  return(
+    <>
+      <p>Number of exercises {total}</p>
+    </>
+  )
+}
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts:[
+            {
+              name: 'Fundamentals of React',
+              exercises: 10
+            },
+
+            {
+              name: 'Using props to pass data',
+              exercises : 7
+            },
+            {
+              name: 'State of a component',
+              exercises: 14
+            }
+          ]
+        }
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course}/>
+      <Total parts={course}/>
+    </div>
+  )
+}
+
+export default App
